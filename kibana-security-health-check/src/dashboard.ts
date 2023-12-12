1ab2bdb25f264a824557ff25eb61c6f5a867494b
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2023-12-12 18:11:20 +0100

    feat(kibana-security-health-check): add support for "dashboard" content extractor script

---
+++ kibana-security-health-check/src/dashboard.ts
@@ -0,0 +1,53 @@
+interface Meta {
+  lastRevisionId: string;
+}
+
+const META_REGEX = /\(https:\/\/meta.secutils.dev\/(.+)\)/gm;
+
+interface Params {
+  targetContentTrackerId: string;
+  targetCspShareId: string;
+  credentials: { username: string; password: string };
+}
+
+interface WebPageContentRevision {
+  id: string;
+  data: string;
+  createdAt: number;
+}
+
+export async function run(previousContent: string | undefined, params: Params): Promise<string> {
+  const previousMeta = previousContent ? extractMeta(previousContent) : undefined;
+
+  // Fetch tracker history.
+  const revisions = (await (
+    await fetch(`${location.origin}/api/utils/web_scraping/content/${params.targetContentTrackerId}/history`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${btoa(`${params.credentials.username}:${params.credentials.password}`)}`,
+      },
+    })
+  ).json()) as WebPageContentRevision[];
+
+  const meta: Meta = {
+    lastRevisionId: revisions.length > 0 ? revisions[revisions.length - 1].id : '',
+  };
+
+  const state = `## Dashboard
+* Content Security Policy: ${meta.lastRevisionId === previousMeta?.lastRevisionId ? '✅' : '⚠'} ([view](${
+    location.origin
+  }/ws/web_security__csp__policies?x-user-share-id=${params.targetCspShareId}))
+`;
+
+  return prependMeta(state, meta);
+}
+
+// HACK: Prepend the meta to the markdown content as part of the link to not clutter the markdown.
+// NOTE: There should be a built-in way to specify metadata and user-facing content separately.
+function prependMeta(markdownState: string, meta: Meta): string {
+  return `[META](https://meta.secutils.dev/${encodeURIComponent(JSON.stringify(meta))})${markdownState}`;
+}
+
+function extractMeta(markdownState: string): Meta {
+  return JSON.parse(decodeURIComponent(Array.from(markdownState.matchAll(META_REGEX))[0][1])) as Meta;
+}
--- kibana-security-health-check/src/index.ts
+++ kibana-security-health-check/src/index.ts
@@ -1,88 +1,19 @@
-interface State {
-  headers: Record<string, string>;
-  resources: {
-    external: ExternalWebPageResource[];
-    internalCount: number;
-    internalTotalSize: string;
-  };
-  injectedMetadata: Record<string, unknown>;
-}
+export { run as dashboardRun } from './dashboard';
+export { run as trackerRun } from './tracker';
 
-interface ExternalWebPageResource extends Omit<WebPageResource, 'data'> {
-  size: string;
-}
-
-interface WebPageResource {
+export interface WebPageResource {
   url: string;
   data: string;
   type: 'script' | 'stylesheet';
 }
 
-const TRACKED_RESPONSE_HEADERS = [
-  'content-security-policy',
-  'cross-origin-opener-policy',
-  'permissions-policy',
-  'referrer-policy',
-  'strict-transport-security',
-  'x-content-type-options',
-  'x-found-handling-cluster',
-  'x-frame-options',
-];
-
-const TRACKED_INJECTED_METADATA = ['anonymousStatusPage', 'clusterInfo', 'csp', 'env', 'externalUrl'];
-
-export async function run(
-  previousContent: State | undefined,
-  remoteResources: WebPageResource[],
-  responseHeaders: Record<string, string>,
-): Promise<State> {
-  const { externalResources, internalResources } = remoteResources.reduce(
-    (acc, resource) => {
-      if (resource.url.startsWith(location.origin)) {
-        acc.internalResources.push(resource);
-      } else {
-        acc.externalResources.push(resource);
-      }
-      return acc;
-    },
-    { externalResources: [] as WebPageResource[], internalResources: [] as WebPageResource[] },
-  );
-
-  // Fetch the content of the page.
-  const dom = new DOMParser().parseFromString(await (await fetch(location.href)).text(), 'text/html');
-  const injectedMetadata = JSON.parse(
-    dom.querySelector('kbn-injected-metadata')?.getAttribute('data') ?? '{}',
-  ) as Record<string, unknown>;
-
-  return {
-    headers: Object.fromEntries(
-      Object.entries(responseHeaders).filter(([key]) => TRACKED_RESPONSE_HEADERS.includes(key.toLowerCase())),
-    ),
-    resources: {
-      external: externalResources.map((resource) => ({
-        url: resource.url,
-        type: resource.type,
-        size: formatBytes(resource.data.length, 3),
-      })),
-      internalCount: internalResources.length,
-      internalTotalSize: formatBytes(
-        internalResources.reduce((sum, res) => sum + res.data.length, 0),
-        3,
-      ),
-    },
-    injectedMetadata: Object.fromEntries(
-      Object.entries(injectedMetadata).filter(([key]) => TRACKED_INJECTED_METADATA.includes(key)),
-    ),
-  };
-}
-function formatBytes(bytes: number, decimals: number) {
-  if (bytes == 0) {
-    return '0 Bytes';
-  }
-
-  const k = 1024,
-    dm = decimals || 2,
-    sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
-    i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+export interface SecurityResponseHeaders {
+  'content-security-policy': string;
+  'cross-origin-opener-policy': string;
+  'permissions-policy': string;
+  'referrer-policy': string;
+  'strict-transport-security': string;
+  'x-content-type-options': string;
+  'x-found-handling-cluster': string;
+  'x-frame-options': string;
 }
