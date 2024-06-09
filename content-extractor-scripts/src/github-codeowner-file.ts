0714c9cd8a866a59fb84d30a4a3e802a8666fbaa
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2024-06-09 04:06:31 +0300

    fix: sort result of `github-codeowner-file` content extractor script by path

--- content-extractor-scripts/src/github-codeowner-file.ts
+++ content-extractor-scripts/src/github-codeowner-file.ts
@@ -5,7 +5,7 @@ export async function run(context: WebPageContext, url: string, teams: string[])
 
   const rows: Array<Array<string | null | undefined>> = [['Owners', 'Path']];
   for (const line of lines) {
-    const [path, owners] = line.split(' ');
+    const [path, owners] = line.split(' ').sort();
     if (owners && teams.some((team) => owners.includes(team))) {
       rows.push([owners, path]);
     }
