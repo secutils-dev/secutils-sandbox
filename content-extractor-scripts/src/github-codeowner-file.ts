a9591fad2285257e58e5741de293836a59b07f7a
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2024-06-09 13:46:45 +0300

    feat: support GitHub API token for `github-codeowner-file` content extractor script

--- content-extractor-scripts/src/github-codeowner-file.ts
+++ content-extractor-scripts/src/github-codeowner-file.ts
@@ -1,23 +1,40 @@
+import type { Endpoints } from '@octokit/types';
+
 import type { WebPageContext } from './types';
 
-export async function run(context: WebPageContext, owner: string, repo: string, teams: string[]): Promise<string> {
+export async function run(
+  context: WebPageContext,
+  owner: string,
+  repo: string,
+  teams: string[],
+  apiToken?: string,
+): Promise<string> {
   const codeOwnersUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/.github/CODEOWNERS`;
   const lines = (await fetch(codeOwnersUrl).then((response) => response.text())).split('\n') ?? [];
+
+  // Use API token if provided to avoid rate limiting (GitHub allows only 60 API requests per hour without token)
+  // https://docs.github.com/en/rest/using-the-rest-api/rate-limits-for-the-rest-api?apiVersion=2022-11-28.
+  const headers: Record<string, string> = apiToken
+    ? { Authorization: `Bearer ${apiToken}`, 'X-GitHub-Api-Version': '2022-11-28' }
+    : { 'X-GitHub-Api-Version': '2022-11-28' };
 
   const getCommitLink = async (path: string) => {
     try {
       const commits = (await fetch(
         `https://api.github.com/repos/${owner}/${repo}/commits?path=${encodeURIComponent(path)}&per_page=1`,
-      ).then((response) => response.json())) as Array<{
-        html_url: string;
-        commit: { author: { date: string; name: string } };
-      }>;
+        { headers },
+      ).then((response) => response.json())) as Endpoints['GET /repos/{owner}/{repo}/commits']['response']['data'];
       if (commits.length === 0) {
         return 'N/A (no commits found)';
       }
-      return `[${commits[0].commit.author.date} by ${commits[0].commit.author.name}](${commits[0].html_url})`;
-    } catch {
-      return 'N/A (error fetching commit)';
+      const topCommit = commits[0];
+      const commitLabel =
+        topCommit.commit.author?.name && topCommit.commit.author?.date
+          ? `${topCommit.commit.author.name} on ${topCommit.commit.author.date}`
+          : topCommit.sha.slice(6);
+      return `[${commitLabel}](${topCommit.html_url})`;
+    } catch (err) {
+      return `N/A (${(err as Error).message ?? 'unknown error'})`;
     }
   };
 
