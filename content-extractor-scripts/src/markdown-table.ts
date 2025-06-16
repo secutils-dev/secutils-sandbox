79f95a81ecf8e314c3ce8267abc8ff108219ca69
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2025-06-16 03:04:33 +0300

    fix: use proper Retrack `execute` method signature

--- content-extractor-scripts/src/markdown-table.ts
+++ content-extractor-scripts/src/markdown-table.ts
@@ -1,12 +1,15 @@
 import { markdownTable } from 'markdown-table';
+import type { Page } from 'playwright-core';
 
 import type { PageContext } from './types';
 
-export async function execute(context: PageContext): Promise<string> {
+export async function execute(page: Page, context: PageContext): Promise<string> {
   return markdownTable(
     [
+      ['Page title', await page.title()],
       ['Has previous content?', context.previousContent ? ':white_check_mark:' : ':x:'],
       ['Params', JSON.stringify(context.params)],
+      ['Tags', JSON.stringify(context.tags)],
       ['Current time', new Date().toISOString()],
     ],
     { align: ['l', 'c'] },
